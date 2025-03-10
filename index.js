fc628ccdff60568ab17ef9728db1559b18e0484d
beansprout <150442539+bea-nsprout@users.noreply.github.com>
2025-03-11 02:56:16 +0800

    index: in localhost now

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI; // use MONGO_URI from .env
+
+// Middleware
+app.use(express.json()); // Parse JSON request bodies
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from public directry
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+/*
+honestly, not sure if that should be used, or remove dotenv and use
+mongoose.connect('mongodb://localhost/galleryDB')
+ */
+
+// Routes
+// const establishmentRoutes = require('./controllers/establishmentController');
+// app.use('/api', establishmentRoutes);
+
+// Serve HTML files from views/HTML
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/HTML/index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+});
