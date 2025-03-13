42dcc41f76127ef1306cd04b9ec98893610b0113
Kean Carvin M. Lamadrid <kean.lamadrid@gmail.com>
2025-03-13 20:45:33 +0800

    restoProfile-hbs-stuff

--- index.js
+++ index.js
@@ -2,12 +2,20 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const dotenv = require('dotenv');
+const hbs = require('hbs');
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI; // use MONGO_URI from .env
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+//old bea code const MONGO_URI = process.env.MONGO_URI; // use MONGO_URI from .env
+
+// Set up Handlebars
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Middleware
 app.use(express.json()); // Parse JSON request bodies
@@ -38,9 +46,40 @@ app.get('/restoList', (req, res) => {
 app.get('/sign-up', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/HTML/sign-up.html'));
 });
+/* old bea code
 app.get('/restoProfiles/:resto', (req, res) => {
     res.sendFile(path.join(__dirname, `views/HTML/restoProfiles/${resto}.html`));
 });
+*/
+const { User, Establishment, Review } = require('./database/models/model');
+
+app.get('/restoProfile/:id', async (req, res) => {
+    try {
+        const establishment = await Establishment.findById(req.params.id)
+            .populate({
+                path: 'reviews',
+                populate: {
+                    path: 'userId', // Get reviewer info
+                    select: 'username avatar'
+                }
+            })
+            .lean();
+
+        const menu = await Menu.findOne({ establishmentId: req.params.id }).lean();
+        const photos = await Photo.find({ establishmentId: req.params.id }).lean();
+
+        if (!establishment) {
+            return res.status(404).send('Establishment not found');
+        }
+
+        res.render('restoProfile', { establishment, menu, photos });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
+    }
+});
+
+
 app.get('/userProfiles/:user', (req, res) => {
     res.sendFile(path.join(__dirname, `views/HTML/userProfiles/${user}.html`));
 });
